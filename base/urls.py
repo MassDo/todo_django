a564c3cf2e0b8cdf2089481e1d2ca193e29591f6
massdo <dorian_massoulier@hotmail.com>
2021-03-18 12:19:05 +0100

    User can create and update task, and login

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLogin
 
 # Prolongement de '/tasks/'
 
 urlpatterns = [
     path('', TaskList.as_view(), name='task-list'),
     path('create/', TaskCreate.as_view(), name='task-create' ),
-    path('<str:pk>/', TaskDetail.as_view(), name='task-detail'),
+    path('login/', CustomLogin.as_view(), name='task-login' ),
+    path('update/<int:pk>/', TaskUpdate.as_view(), name='task-update' ),
+    path('delete/<int:pk>/', TaskDelete.as_view(), name='task-delete' ),
+    path('<int:pk>/', TaskDetail.as_view(), name='task-detail'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 from django.views.generic.list import ListView
-from django.views.generic import DetailView
-from django.views.generic import CreateView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView
 
 from .models import Task
 
 # def task_list(request):
 #     return HttpResponse('teest')
+
+class CustomLogin(LoginView):
+    template_name = 'base/login.html'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('task-list')
 
 class TaskList(ListView):
     model = Task
@@ -19,7 +28,20 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
 
+
 class TaskCreate(CreateView):
     model = Task
     fields = '__all__'
+    success_url = reverse_lazy('task-list')
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('task-list')
+
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
     success_url = reverse_lazy('task-list')
